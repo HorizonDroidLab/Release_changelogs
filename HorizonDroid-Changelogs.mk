08686817d49cac5a52324c928db8063be93dd231
SuperXorn <151366950+ryzenforce990@users.noreply.github.com>
2024-02-13 19:23:09 +0700

    HorizonDroid: v2.1 " Vega "

---
+++ HorizonDroid-Changelogs.mk
@@ -0,0 +1,43 @@
+HorizonDroid Android 14 v2.1 presents: "Vega"
+
+Changelog:
+• Merged Feb 2024 security patch
+• Add New Desain HorizonLab For Customizations
+• Add Bootnimation
+• Add Adaptive refresh rate display
+• Add Colored statusbar icons
+• Add Colored qs notification icons
+• Add Roaming indicator optional
+• Add 4G icon instead LTE
+• Add Bluetooth timeout feature
+• Add Wi-Fi timeout feature
+• Add Data disabled indicator icons
+• Add Screen off animation
+• Add Lockscreen media art
+• Add Android P (Pie) Animations
+• Add Support for black theme
+• Add PowerMenu advanced reboot options
+• Add NetworkTraffic
+• Add Support for window ignore secure
+• Add Circle thumb at the end of seekbar
+• Add Dual-tone light and dark themes for QS
+• Add Less boring heads up option
+• Add Caffeine QS Tile
+• Add Add navbar layout inversion tuning
+• Add Bluetooth battery level
+• Add Add tunables for navbar layout
+• Add Statusbar icons tuner
+• Add More Qs Tile
+• Add Three-fingers-swipe to screenshot 
+• Add Adaptive Playback
+• Add More DNS providers
+• Add Android P Animations
+• Add Intergrate Microsoft Link To Windows 
+• Import illustrations from motorola
+• Import missing animations from UP1A.231005.007.A1
+• Move network traffic to HorizonLab
+• Move advanced reboot toggle to HorizonLab
+• Move black theme toggle to HorizonLab 
+• Move window ignore secure feature to HorizonLab
+• Move toggle for secure tiles on keyguard to HorizonLab 
+• Move Hide power menu on secure lockscreen to HorizonLab
