8413aefe0ca1745797ef1edc2b0e983d5f931896
SuperXorn <151366950+ryzenforce990@users.noreply.github.com>
2024-02-24 04:29:44 +0700

    Changelog For v2.2 (Elysium)

--- HorizonDroid-Changelogs.mk
+++ HorizonDroid-Changelogs.mk
@@ -1,43 +1,29 @@
-HorizonDroid Android 14 v2.1 presents: "Vega"
+==============================================
+HorizonDroid Android 14 presents: "Elysium"
+==============================================
+Changelogs:
+• Add Icon shapes, icon packs, fonts Customisation 
+• Add Signal and wifi icon style theming
+• Add GamesProp
+• Add Battery Landscape
+• Add Landscape iOS battery styles
+• Add VoLTE & VoWiFi Icons
+• Add Custom statusbar logo customizations
+• Add Add Brightness TextView
+• Add Pulse ambient edge lightning
+• Add Wifi standard icon
+• Add Data icon style
+• Add statusbar NFC icon
+• Introduce dynamic VoLTE & VoWiFi icons
+• Introduce contextual dashboard messages
+• Improve design of "VO" icon
+• Redesign seekbar
+• Add deep sleep info to uptime preference
+• Make popup menus more rounded
+• Make QS footer rounded 
 
-Changelog:
-• Merged Feb 2024 security patch
-• Add New Desain HorizonLab For Customizations
-• Add Bootnimation
-• Add Adaptive refresh rate display
-• Add Colored statusbar icons
-• Add Colored qs notification icons
-• Add Roaming indicator optional
-• Add 4G icon instead LTE
-• Add Bluetooth timeout feature
-• Add Wi-Fi timeout feature
-• Add Data disabled indicator icons
-• Add Screen off animation
-• Add Lockscreen media art
-• Add Android P (Pie) Animations
-• Add Support for black theme
-• Add PowerMenu advanced reboot options
-• Add NetworkTraffic
-• Add Support for window ignore secure
-• Add Circle thumb at the end of seekbar
-• Add Dual-tone light and dark themes for QS
-• Add Less boring heads up option
-• Add Caffeine QS Tile
-• Add Add navbar layout inversion tuning
-• Add Bluetooth battery level
-• Add Add tunables for navbar layout
-• Add Statusbar icons tuner
-• Add More Qs Tile
-• Add Three-fingers-swipe to screenshot 
-• Add Adaptive Playback
-• Add More DNS providers
-• Add Android P Animations
-• Add Intergrate Microsoft Link To Windows 
-• Import illustrations from motorola
-• Import missing animations from UP1A.231005.007.A1
-• Move network traffic to HorizonLab
-• Move advanced reboot toggle to HorizonLab
-• Move black theme toggle to HorizonLab 
-• Move window ignore secure feature to HorizonLab
-• Move toggle for secure tiles on keyguard to HorizonLab 
-• Move Hide power menu on secure lockscreen to HorizonLab
+Full changelog is here.
+https://github.com/HorizonDroidLab
+
+Main Chatroom: @horizondroidchatroom 
+Device Updates: @horizondroidupdates
